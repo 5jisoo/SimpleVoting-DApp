175094b653a02b90ea2b717e03181e9880cf781e
Lucy Oh <56earls@gmail.com>
2024-12-31 15:25:10 +0900

    Feat: register voters

---
+++ SimpleVotingWeb/scripts/registerVoter.js
@@ -0,0 +1,68 @@
+import { SimpleVoting } from "./setting";
+
+function registerVoter() {
+  $("#voterRegistrationMessage").html("");
+
+  var adminAddress = $("#adminAddress").val();
+  var voterToRegister = $("#voterAddress").val();
+
+  SimpleVoting.deployed()
+    .then((instance) => instance.isAdministrator(adminAddress))
+    .then((isAdministrator) => {
+      // 관리자 권한 확인
+      if (isAdministrator) {
+        return SimpleVoting.deployed()
+          .then((instance) => instance.isRegisteredVoter(voterToRegister))
+          .then((isRegisteredVoter) => {
+            // 이미 등록된 유권자
+            if (isRegisteredVoter)
+              $("#voterRegistrationMessage").html(
+                "The voter is already registered"
+              );
+            else {
+              return SimpleVoting.deployed()
+                .then((instance) => instance.getWorkflowStatus())
+                .then((workflowStatus) => {
+                  if (workflowStatus > 0)
+                    $("#voterRegistrationMessage").html(
+                      "Voters registration has already ended"
+                    );
+                  else {
+                    SimpleVoting.deployed()
+                      .then((instance) =>
+                        instance.registerVoter(voterToRegister, {
+                          from: adminAddress,
+                          gas: 200000,
+                        })
+                      )
+                      .catch((e) => $("#voterRegistrationMessage").html(e));
+                  }
+                });
+            }
+          });
+      } else {
+        $("#voterRegistrationMessage").html(
+          "The given address does not correspond to the administrator"
+        );
+      }
+    });
+}
+
+function checkVoterRegistration() {
+  $("#registrationVerificationMessage").html("");
+
+  var address = $("#address").val();
+
+  SimpleVoting.deployed()
+    .then((instance) => instance.isRegisteredVoter(address))
+    .then((isRegisteredVoter) => {
+      if (isRegisteredVoter)
+        $("#registrationVerificationMessage").html(
+          "This is a registered voter"
+        );
+      else
+        $("#registrationVerificationMessage").html(
+          "This is NOT a registered voter"
+        );
+    });
+}
